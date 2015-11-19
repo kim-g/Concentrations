6d28bcb4540163c2b10f7bc7d5b408a2511b9eca
Григорий Ким <kim-g@yandex.ru>
2015-11-19 16:08:38 +0500

    Окно навески сделано

--- Concentrations/Mass.Designer.cs
+++ Concentrations/Mass.Designer.cs
@@ -29,25 +29,202 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Vol = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.MmEdit = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.CaEdit = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.CxEdit = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ResultLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(126, 28);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(18, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(549, 60);
             this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.label1.Text = "Расчёт навески по атомному весу молекулы, объёму колбы и желаемой концентрации";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(18, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Объём";
+            // 
+            // Vol
+            // 
+            this.Vol.Location = new System.Drawing.Point(85, 84);
+            this.Vol.Name = "Vol";
+            this.Vol.Size = new System.Drawing.Size(73, 26);
+            this.Vol.TabIndex = 2;
+            this.Vol.Text = "0";
+            this.Vol.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(164, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "мл. ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(259, 87);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(194, 20);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Желаемый объём колбы";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(259, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(293, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Масса молекулы в атомных единицах";
+            // 
+            // MmEdit
+            // 
+            this.MmEdit.Location = new System.Drawing.Point(85, 116);
+            this.MmEdit.Name = "MmEdit";
+            this.MmEdit.Size = new System.Drawing.Size(73, 26);
+            this.MmEdit.TabIndex = 6;
+            this.MmEdit.Text = "0";
+            this.MmEdit.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(18, 119);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 20);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Mm";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(259, 164);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(272, 20);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Желаемая концентрация  (Моль/л)";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(149, 164);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(34, 20);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "x10";
+            // 
+            // CaEdit
+            // 
+            this.CaEdit.Location = new System.Drawing.Point(85, 161);
+            this.CaEdit.Name = "CaEdit";
+            this.CaEdit.Size = new System.Drawing.Size(58, 26);
+            this.CaEdit.TabIndex = 10;
+            this.CaEdit.Text = "1";
+            this.CaEdit.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(18, 164);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(50, 20);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Конц.";
+            // 
+            // CxEdit
+            // 
+            this.CxEdit.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CxEdit.Location = new System.Drawing.Point(189, 151);
+            this.CxEdit.Name = "CxEdit";
+            this.CxEdit.Size = new System.Drawing.Size(45, 23);
+            this.CxEdit.TabIndex = 13;
+            this.CxEdit.Text = "0";
+            this.CxEdit.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label10.Location = new System.Drawing.Point(179, 154);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(13, 17);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "-";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 193);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(530, 29);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Расчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ResultLabel
+            // 
+            this.ResultLabel.AutoSize = true;
+            this.ResultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ResultLabel.Location = new System.Drawing.Point(19, 237);
+            this.ResultLabel.Name = "ResultLabel";
+            this.ResultLabel.Size = new System.Drawing.Size(0, 20);
+            this.ResultLabel.TabIndex = 16;
             // 
             // Mass
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(432, 286);
+            this.ClientSize = new System.Drawing.Size(580, 289);
+            this.Controls.Add(this.ResultLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.CxEdit);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.CaEdit);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.MmEdit);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Vol);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Mass";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
             this.Text = "Навеска";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Mass_FormClosed);
             this.ResumeLayout(false);
@@ -58,5 +235,20 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Vol;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox MmEdit;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox CaEdit;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox CxEdit;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label ResultLabel;
     }
 }
--- Concentrations/Mass.cs
+++ Concentrations/Mass.cs
@@ -5,8 +5,8 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace Concentrations
 {
@@ -25,6 +25,111 @@ namespace Concentrations
         private void Mass_FormClosed(object sender, FormClosedEventArgs e)
         {
             
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            int SelStart = textBox.SelectionStart;
+            int TextLength = textBox.Text.Length;
+
+            textBox.Text = Regex.Replace(textBox.Text, "[^0-9.,]", "");
+            //textBox1.Text = Regex.Replace(textBox1.Text, "[.,]", separator.ToString());
+            if (TextLength == textBox.Text.Length)
+            { textBox.SelectionStart = SelStart; }
+            else
+            {
+                textBox.SelectionStart = SelStart-1;
+            };
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double volume;
+            double Mm;
+            double Ca;
+            double Cx;
+            char separator = System.Globalization.CultureInfo.CurrentCulture.NumberFormat.CurrencyDecimalSeparator[0];
+
+            //Ввод объёма
+            try
+            {
+                volume = Convert.ToSingle(
+                    Regex.Replace(
+                        Regex.Replace(Vol.Text, "[^0-9.,]", ""), 
+                        "[.,]", separator.ToString()));
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Неправильно введён объём колбы.");
+                return;
+            };
+            if (volume<=0)
+            {
+                MessageBox.Show("Неправильно введён объём колбы.\nОбъём должен быть больше 0!");
+                return;
+            };
+
+            //Ввод молярной массы
+            try
+            {
+                Mm = Convert.ToSingle(
+                    Regex.Replace(
+                        Regex.Replace(MmEdit.Text, "[^0-9.,]", ""),
+                        "[.,]", separator.ToString()));
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Неправильно введена молярная масса.");
+                return;
+            };
+            if (Mm <= 0)
+            {
+                MessageBox.Show("Неправильно введена молярная масса.\nМолярная масса должна быть больше 0!");
+                return;
+            };
+
+            //Ввод концентрации
+            try
+            {
+                Ca = Convert.ToSingle(
+                    Regex.Replace(
+                        Regex.Replace(CaEdit.Text, "[^0-9.,]", ""),
+                        "[.,]", separator.ToString()));
+                Cx = 0 - Convert.ToSingle(
+                    Regex.Replace(
+                        Regex.Replace(CxEdit.Text, "[^0-9.,]", ""),
+                        "[.,]", separator.ToString()));
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Неправильно введена желаемая концентрация.");
+                return;
+            };
+            if (Ca <= 0)
+            {
+                MessageBox.Show("Неправильно введена желаемая концентрация.\nЖелаемая концентрация должна быть больше 0!");
+                return;
+            };
+
+            double Res = volume * Mm * Ca * Math.Pow(10, Cx) / 1000;
+
+            ResultLabel.Text = "g = " + Res.ToString("N5") + " г.";
+
+            Clipboard.SetText(Res.ToString());
+
+
         }
     }
 }
