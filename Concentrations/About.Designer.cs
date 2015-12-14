65ce8975f7da4fd82504b427c8b26c61e7da9117
Григорий Ким <kim-g@yandex.ru>
2015-12-14 16:42:35 +0500

    Применена лицензия BSD. Добавлено окно с текстом лицензии.

--- Concentrations/About.Designer.cs
+++ Concentrations/About.Designer.cs
@@ -34,6 +34,8 @@
             this.label1 = new System.Windows.Forms.Label();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -65,14 +67,14 @@
             this.label1.Location = new System.Drawing.Point(13, 115);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(609, 190);
+            this.label1.Size = new System.Drawing.Size(609, 225);
             this.label1.TabIndex = 2;
             this.label1.Text = resources.GetString("label1.Text");
             // 
             // linkLabel1
             // 
             this.linkLabel1.AutoSize = true;
-            this.linkLabel1.Location = new System.Drawing.Point(410, 235);
+            this.linkLabel1.Location = new System.Drawing.Point(313, 275);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(137, 20);
             this.linkLabel1.TabIndex = 3;
@@ -82,7 +84,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(481, 308);
+            this.button1.Location = new System.Drawing.Point(482, 343);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(141, 28);
             this.button1.TabIndex = 4;
@@ -90,11 +92,34 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(19, 343);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 28);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Лицензия";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(15, 216);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(174, 20);
+            this.linkLabel2.TabIndex = 6;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "kim-g.ru/concentrations";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
             // About
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(635, 348);
+            this.ClientSize = new System.Drawing.Size(635, 388);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.label1);
@@ -123,5 +148,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.LinkLabel linkLabel2;
     }
 }
--- Concentrations/About.cs
+++ Concentrations/About.cs
@@ -1,11 +1,40 @@
-﻿using System;
+﻿/****************************************************************/
+/*                                                              */
+/*         Программа для расчёта навесок и концентраций         */
+/*                          Версия 1.0                          */
+/*                     Модуль «О программе»                     */
+/*                                                              */
+/*                     Автор – Григорий Ким                     */
+/*                      kim-g@ios.uran.ru                       */
+/*    Федеральное государственное бюджетное учреждение науки    */
+/*     Институт органического синтеза им. И.Я. Постовского      */
+/* Уральского отделения Российской академии наук (ИОС УрО РАН)  */
+/*                                                              */
+/*                 Распространяется на условиях                 */
+/*            Berkeley Software Distribution license            */
+/*                                                              */
+/****************************************************************/
+
+
+using System;
 using System.Diagnostics;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace Concentrations
 {
     public partial class About : Form
     {
+        private const string Author = "Grigory A. Kim";
+        private const string Organization = "Institute of Organic Synthesis UB RAS, Ekaterinburg, Russia";
+        private const string Year = "2015";
+        private const string BSD_License = "Copyright (c) <YEAR>, <OWNER>. All rights reserved.\n\n" +
+                                           "Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:\n\n"+
+                                           "Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.\n\n" +
+                                           "Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.\n\n" +
+                                           "Neither the name of the <ORGANIZATION> nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.\n\n" +
+                                           "THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.";
+
         public About()
         {
             InitializeComponent();
@@ -19,6 +48,23 @@ namespace Concentrations
         private void button1_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string TextToShow = Regex.Replace(
+                                                Regex.Replace(
+                                                                Regex.Replace( 
+                                                                                BSD_License, "<ORGANIZATION>", Organization
+                                                                              ) , "<YEAR>", Year
+                                                              ), "<OWNER>", Author + ", " + Organization
+                                              );
+            MessageBox.Show(TextToShow,"BSD License");
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("http://kim-g.ru/concentrations");
         }
     }
 }
